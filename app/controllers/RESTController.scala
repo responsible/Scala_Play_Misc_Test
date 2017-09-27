323d085d025063c3fef6fdea6c556f990eccdc45
shihao <shyan@thoughtworks.com>
2017-09-27 11:33:52 +0800

    [Shihao] refactor auth filter

--- app/controllers/RESTController.scala
+++ app/controllers/RESTController.scala
@@ -2,9 +2,12 @@ package controllers
 
 import javax.inject.{Inject, Singleton}
 
-import model.{Auth, JsonResponse}
+import model.JsonResponse
 import play.api.libs.json.Json
 import play.api.mvc.{AbstractController, ControllerComponents}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 @Singleton
 class RESTController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
@@ -12,10 +15,12 @@ class RESTController @Inject()(cc: ControllerComponents) extends AbstractControl
     Ok(JsonResponse())
   }
 
-  def publicResource = Action { request =>
-    Ok(JsonResponse(data = Json.obj(
-      "id" -> 1,
-      "name" -> "test"
-    )))
+  def publicResource = Action.async {
+    Future {
+      Ok(JsonResponse(data = Json.obj(
+        "id" -> 1,
+        "name" -> "test"
+      )))
+    }
   }
 }
--- app/filters/AuthFilter.scala
+++ app/filters/AuthFilter.scala
@@ -4,33 +4,31 @@ import javax.inject.Inject
 
 import akka.stream.Materializer
 import model.JsonResponse
+import org.apache.commons.codec.binary.Base64
 import play.api.mvc.Results.Unauthorized
 import play.api.mvc._
 
 import scala.concurrent.{ExecutionContext, Future}
 
-class AuthFilter @Inject()(implicit override val mat: Materializer,
-                           exec: ExecutionContext) extends Filter {
-  override def apply(f: (RequestHeader) => Future[Result])(request: RequestHeader) = {
-    val submittedCredentials: Option[List[String]] = for {
-      authHeader <- request.headers.get("Authorization")
-      parts <- authHeader.split(' ').drop(1).headOption
-    } yield new String(org.apache.commons.codec.binary.Base64.decodeBase64(parts.getBytes)).split(':').toList
-
-    if (submittedCredentials.collect {
-      case username :: password :: Nil if check(username, password) =>
-    }.nonEmpty)
-      f(request)
-    else
-      Future.successful(Unauthorized(
+class AuthFilter @Inject()(implicit override val mat: Materializer, exec: ExecutionContext) extends Filter {
+  override def apply(next: (RequestHeader) => Future[Result])(request: RequestHeader) = {
+    getUserAndPassFromHttpAuthHeader(request) match {
+      case Some(username :: password :: Nil) if check(username, password) => next(request)
+      case _ => Future.successful(Unauthorized(
         JsonResponse(
           msg = "Wrong username or password",
           code = 401)
       ).withHeaders("WWW-Authenticate" -> """Basic realm="Secured Area""""))
+    }
   }
 
-  def check(username: String, password: String): Boolean = {
-    if (username == "admin" && password == "123456") true
-    else false
+  private def getUserAndPassFromHttpAuthHeader(request: RequestHeader): Option[List[String]] = {
+    for {
+      authHeader <- request.headers.get("Authorization")
+      parts <- authHeader.split(' ').drop(1).headOption
+    } yield new String(Base64.decodeBase64(parts.getBytes)).split(':').toList
   }
+
+  def check(username: String, password: String): Boolean =
+    username == "admin" && password == "123456"
 }
